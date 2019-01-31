49ab96745c895a38e5c55ecdc5d0f237f6228aa5
John <John.Webb94@gmail.com>
2019-01-30 23:02:38 -0700

    TEMP

--- web_assembly/visualize.c
+++ web_assembly/visualize.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+extern setInterval(int (* callback)());
+
+int runCallback(int (*callback)()) {
+  return callback();
+}
+
+int tick() {
+
+}
+
 int main(int argc, char ** argv) {
   printf("Hello World!\n");
 }
